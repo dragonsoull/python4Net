18f0258140a2fa5cbacd3ceef124ad711be8cfed
dragonsoull <zw@linuxoak.com>
2024-12-29 14:33:04 -0800

    latest modification

--- fakeapi.py
+++ fakeapi.py
@@ -6,3 +6,7 @@ url="https://fakestoreapi.com/products/1"
 
 response = requests.get(url)
 print(response.json())
+print("")
+url="https://postman-echo.com/basic-auth"
+response=requests.get(url,auth=("postman","password"))
+print(response.json())
